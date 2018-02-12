1f4d8f2ec2c57918d7609bd7dcc25d1be056e764
mk <majd.khoury93@gmail.com>
2018-02-11 21:31:25 -0500

    Added iterable array of catnames

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ var ViewModel = function(){
   this.name = ko.observable("Memo");
   this.clickCount = ko.observable(0);
   this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
+  this.nicknames = ko.observableArray([
+      { name: "Meowster"},
+      { name: "Charles"},
+      { name: "Denise"}
+  ]);
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
